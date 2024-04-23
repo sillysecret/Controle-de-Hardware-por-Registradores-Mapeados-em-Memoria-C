7f2bffc740759ce6270bf1996e947c31251def05
João Pedro da Silva Kriger <128426379+sillysecret@users.noreply.github.com>
2024-04-23 19:15:47 -0300

    Update registers.c

--- registers.c
+++ registers.c
@@ -60,10 +60,12 @@ int main() {
 
     unsigned short *base_address = (unsigned short *)map;
     unsigned short *r0 = base_address + 0x00;
-    printf("Initial value of R0: 0x%02x\n", *r0);
+    unsigned short *r1 = base_address + 0x01;
+    printf("Current value of R0: 0x%02x\n", *r0);
+    printf("Current value of R1: 0x%02x\n", *r1);
 
     // Write a new value to R0
-    *r0 = 0x01;
+    *r0 = *r0 | 0x00;
 
     // Release resources
     if (registers_release(map, FILE_SIZE, fd) == -1) {
