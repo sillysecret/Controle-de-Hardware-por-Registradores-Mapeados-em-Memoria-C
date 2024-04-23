0f2c40db32e6bb2fbf40a7e7a23138cd27191b0a
Your Name <you@example.com>
2024-04-23 20:49:49 -0300

    metodos de interacao com display

--- registers.c
+++ registers.c
@@ -59,18 +59,67 @@ int main() {
     }
 
     unsigned short *base_address = (unsigned short *)map;
+
     unsigned short *r0 = base_address + 0x00;
     unsigned short *r1 = base_address + 0x01;
+    unsigned short *r2 = base_address + 0x02;
+    unsigned short *r3 = base_address + 0x03;
+
+    int aux;
+    unsigned int mask;
+
     printf("Current value of R0: 0x%02x\n", *r0);
     printf("Current value of R1: 0x%02x\n", *r1);
+    do{
+        printf("display on/off = 1\n");
+        printf("display exib = 2,3,4,5\n");
+        scanf("%d",&aux);
+        if(aux == 1){
+            *r0 = *r0 ^ 0x01;
+        }
 
-    // Write a new value to R0
-    *r0 = *r0 | 0x00;
+        
+        if(aux == 2){
+            //estatico
+            mask = ~(0x01<<1);
+            *r0 = *r0 & mask;
+            mask = ~(0x01<<2);
+            *r0 = *r0 & mask;
+            
+        }
+
+        if(aux == 3){
+            //deslizante
+            mask = (0x01<<1);
+            *r0 = *r0 | mask;
+            mask = ~(0x01<<2);
+            *r0 = *r0 & mask;
+        }
+        
+        if(aux == 4){
+            //piscante
+            mask = (0x01<<2);
+            *r0 = *r0 | mask;
+            mask = ~(0x01<<1);
+            *r0 = *r0 & mask;
+        }
+        
+        if(aux == 5){
+            //deslizante e piscante
+            mask = (0x01<<1);
+            *r0 = *r0 | mask;
+            mask = (0x01<<2);
+            *r0 = *r0 | mask;
+        }
+
+    }while (1);
 
     // Release resources
     if (registers_release(map, FILE_SIZE, fd) == -1) {
         return EXIT_FAILURE;
     }
+
+
 
     return EXIT_SUCCESS;
 }
